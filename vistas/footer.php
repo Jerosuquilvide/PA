c6839d87a42bf71faa883e1e597839ef7423eb47
Jerosuquilvide <jeronimosuquilvide@gmail.com>
2022-09-28 12:33:56 -0300

    corregi el footer

--- vistas/footer.php
+++ vistas/footer.php
@@ -1,7 +1,7 @@
     <!-- Pie -->
 
-    <footer style="position: fixed; bottom: 0px; width: 100%; height: 60px; background-color: black;">
-        <h3 class="text-center text-white">&copy 2022</h3>
+    <footer style="position: fixed; bottom: 0px; width: 100%; height: 60px; background-color: black; padding-top:10px">
+        <h3 class="text-center text-white">Miller Marcos ~ Jerónimo Suquilvide ~ &copy 2022</h3>
   </footer>
 
     </body>
